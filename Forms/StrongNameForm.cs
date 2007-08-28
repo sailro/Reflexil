cd9680a53f582dfe02210cff746b994c18a18fc0
sailro <sailro@245f54d4-4a38-0410-b06a-b1304f9c06f5>
2007-08-28 15:46:50 +0000

    Strong name support
    git-svn-id: http://svn.code.sf.net/p/reflexil/code@13 245f54d4-4a38-0410-b06a-b1304f9c06f5

--- Forms/StrongNameForm.cs
+++ Forms/StrongNameForm.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using Reflexil.Utils;
+using System.IO;
 #endregion
 
 namespace Reflexil.Forms
@@ -33,13 +34,29 @@ namespace Reflexil.Forms
         {
             if (OpenFileDialog.ShowDialog() == DialogResult.OK)
             {
-                StrongNameUtility.Resign(m_assemblyfile, OpenFileDialog.FileName);
+                if (!StrongNameUtility.Resign(m_assemblyfile, OpenFileDialog.FileName, Path.GetExtension(OpenFileDialog.FileName).ToLower() == ".pfx"))
+                {
+                    MessageBox.Show("Re-signing fails, check that the supplied key is valid and match the original assembly key", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Re-signing succeeds", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    DialogResult = DialogResult.OK;
+                }
             }
         }
 
         private void Register_Click(object sender, EventArgs e)
         {
-            StrongNameUtility.RegisterForVerificationSkipping(m_assemblyfile);
+            if (!StrongNameUtility.RegisterForVerificationSkipping(m_assemblyfile))
+            {
+                MessageBox.Show("Registering for verification skipping fails", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Registering for verification skipping succeeds", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DialogResult = DialogResult.OK;
+            }
         }
         #endregion
 
@@ -47,6 +64,17 @@ namespace Reflexil.Forms
         public StrongNameForm()
         {
             InitializeComponent();
+            if (!StrongNameUtility.StrongNameToolPresent)
+            {
+                SnToolNotFound.Visible = true;
+                Note.Visible = false;
+                Resign.Enabled = false;
+                Register.Enabled = false;
+            }
+            else
+            {
+                SnToolNotFound.Visible = false;
+            }
         }
         #endregion
 
--- Utils/StrongNameUtility.cs
+++ Utils/StrongNameUtility.cs
@@ -7,6 +7,7 @@ using Mono.Cecil.Cil;
 using Microsoft.Win32;
 using System.IO;
 using System.Diagnostics;
+using System.Reflection;
 #endregion
 
 namespace Reflexil.Utils
@@ -14,42 +15,97 @@ namespace Reflexil.Utils
 	static class StrongNameUtility
     {
 
+        #region " Constants "
+        const string SN_FILENAME = "sn.exe";
+        const string PATH_ENV_VAR = "PATH";
+        const string SDK_PATH_REGKEY = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\.NETFramework";
+        const string SDK_PATH_REGVALUE = "sdkInstallRootv2.0";
+        const string SDK_BIN_PATH = "Bin";
+        #endregion
+
         #region " Properties "
-        public static string ExecutablePath
+        public static bool StrongNameToolPresent
         {
             get
             {
-                string snpath = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\.NETFramework", "sdkInstallRootv2.0", string.Empty).ToString();
-                snpath = Path.Combine(snpath, "Bin");
-                if (!Directory.Exists(snpath))
+                return File.Exists(StrongNameToolFilename);
+            }
+        }
+
+        private static string StrongNameToolFilename
+        {
+            get
+            {
+                string executable = Path.Combine(Path.GetDirectoryName(typeof(StrongNameUtility).Assembly.Location), SN_FILENAME);
+                if (!File.Exists(executable))
                 {
-                   snpath = string.Empty;
+                    executable = null;
                 }
-                snpath = Path.Combine(snpath, "sn.exe");
-                return snpath;
+                else
+                {
+                    return executable;
+                }
+                
+                
+                string path = System.Environment.GetEnvironmentVariable(PATH_ENV_VAR);
+                foreach (string item in path.Split(Path.PathSeparator))
+                {
+                    if (File.Exists(Path.Combine(item, SN_FILENAME)))
+                    {
+                        executable = Path.Combine(item, SN_FILENAME);
+                        break;
+                    }
+                }
+
+                if (executable == null)
+                {
+                    try
+                    {
+                        executable = Registry.GetValue(SDK_PATH_REGKEY, SDK_PATH_REGVALUE, string.Empty).ToString();
+                        executable = Path.Combine(executable, SDK_BIN_PATH);
+                        if (!Directory.Exists(executable))
+                        {
+                            executable = string.Empty;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        executable = string.Empty;
+                    }
+                    executable = Path.Combine(executable, SN_FILENAME);
+                }
+
+                return executable;
             }
         }
         #endregion
 
         #region " Methods "
-        private static bool CallStrongNameUtility(string arguments)
+        private static bool CallStrongNameUtility(string arguments, bool show)
         {
-            ProcessStartInfo startInfo = new ProcessStartInfo(ExecutablePath, arguments);
-            startInfo.CreateNoWindow = true;
-            startInfo.UseShellExecute = false;
-            Process snProcess = Process.Start(startInfo);
-            snProcess.WaitForExit();
-            return snProcess.ExitCode == 0;
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo(StrongNameToolFilename, arguments);
+                startInfo.CreateNoWindow = !show;
+                startInfo.UseShellExecute = false;
+                Process snProcess = Process.Start(startInfo);
+                snProcess.WaitForExit();
+                return snProcess.ExitCode == 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
-        public static bool Resign(string assemblyfile, string keyfile)
+        public static bool Resign(string assemblyfile, string keyfile, bool show)
         {
-            return CallStrongNameUtility(string.Format("-R \"{0}\" \"{1}\"", assemblyfile, keyfile));
+            return CallStrongNameUtility(string.Format("-R \"{0}\" \"{1}\"", assemblyfile, keyfile), show);
         }
 
         public static bool RegisterForVerificationSkipping(string assemblyfile)
         {
-            return CallStrongNameUtility(string.Format("-Vr \"{0}\"", assemblyfile));
+            return CallStrongNameUtility(string.Format("-Vr \"{0}\"", assemblyfile), false);
         }
         #endregion
 
