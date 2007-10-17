c12efb1a855bc61ad69bdccb627ed9c93088fdf2
Sebastien LEBRETON <sebastien@lebreton.fr>
2007-10-17 16:16:52 +0000

    Variable editor
    git-svn-id: http://svn.code.sf.net/p/reflexil/code@44 245f54d4-4a38-0410-b06a-b1304f9c06f5

---
+++ Forms/EditVariableForm.cs
@@ -0,0 +1,51 @@
+
+#region " Imports "
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+#endregion
+
+namespace Reflexil.Forms
+{
+    public partial class EditVariableForm : Reflexil.Forms.VariableForm
+    {
+
+        #region " Methods "
+        public EditVariableForm()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region " Events "
+        private void ButUpdate_Click(object sender, EventArgs e)
+        {
+            if (IsFormComplete)
+            {
+                SelectedVariable.Name = ItemName.Text;
+                SelectedVariable.VariableType = TypeSpecificationEditor.SelectedTypeReference;
+
+                DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                DialogResult = DialogResult.None;
+            }
+        }
+
+        private void EditVariableForm_Load(object sender, EventArgs e)
+        {
+            ItemName.Text = SelectedVariable.Name;
+            TypeSpecificationEditor.SelectedTypeReference = SelectedVariable.VariableType;
+        }
+        #endregion
+
+    }
+}
+
---
+++ Forms/VariableForm.Designer.cs
@@ -0,0 +1,53 @@
+namespace Reflexil.Forms
+{
+    partial class VariableForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TypeSpecificationEditor
+            // 
+            this.TypeSpecificationEditor.AllowReference = false;
+            // 
+            // VariableForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(536, 127);
+            this.Name = "VariableForm";
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+    }
+}
