0b0eb08583b1b12c21ca9d373fe20ce984626ba7
Sebastien LEBRETON <sebastien@lebreton.fr>
2007-10-17 16:15:45 +0000

    New dedicated attributes controls
    git-svn-id: http://svn.code.sf.net/p/reflexil/code@42 245f54d4-4a38-0410-b06a-b1304f9c06f5

---
+++ Editors/Attributes/AttributesControl.cs
@@ -0,0 +1,79 @@
+
+#region " Imports "
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using Reflexil.Wrappers;
+#endregion
+
+namespace Reflexil.Editors
+{
+
+    public partial class AttributesControl: UserControl
+    {
+
+        #region " Fields "
+        private object m_item = null;
+        private bool m_refreshingFlags = false;
+        #endregion
+
+        #region " Methods "
+        public AttributesControl()
+        {
+            InitializeComponent();
+        }
+
+        public void Bind(object item, Dictionary<string, string> prefixes)
+        {
+            Flags.Items.Clear();
+            if (item != null)
+            {
+                foreach (PropertyInfo pinfo in item.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public))
+                {
+                    if ((pinfo.PropertyType == typeof(bool)) && pinfo.CanRead && pinfo.CanWrite)
+                    {
+                        Flags.Items.Add(new PropertyWrapper(pinfo, prefixes));
+                    }
+                }
+            }
+            m_item = item;
+            RefreshFlags();
+        }
+
+        public void RefreshFlags()
+        {
+            if (m_item == null)
+            {
+                Flags.ClearSelected();
+            }
+            else
+            {
+                m_refreshingFlags = true;
+                for (int i = 0; i < Flags.Items.Count; i++)
+                {
+                    PropertyWrapper wrapper = (PropertyWrapper)Flags.Items[i];
+                    Flags.SetItemChecked(i, (bool)wrapper.PropertyInfo.GetValue(m_item, null));
+                }
+                m_refreshingFlags = false;
+            }
+        }
+        #endregion
+
+        #region " Events "
+        private void Flags_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (!m_refreshingFlags && m_item != null)
+            {
+                PropertyWrapper wrapper = (PropertyWrapper)Flags.Items[e.Index];
+                wrapper.PropertyInfo.SetValue(m_item, e.NewValue == CheckState.Checked, null);
+                RefreshFlags();
+            }
+        }
+        #endregion
+	}
+}
