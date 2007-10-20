3a997be24b230d7dca4dd67e3915f769bfdcaec2
Sebastien LEBRETON <sebastien@lebreton.fr>
2007-10-20 20:58:29 +0000

    Method return type editor
    git-svn-id: http://svn.code.sf.net/p/reflexil/code@58 245f54d4-4a38-0410-b06a-b1304f9c06f5

--- Editors/Attributes/MethodAttributesControl.Designer.cs
+++ Editors/Attributes/MethodAttributesControl.Designer.cs
@@ -28,15 +28,21 @@ namespace Reflexil.Editors
 		/// </summary>
 		private void InitializeComponent()
 		{
+            this.components = new System.ComponentModel.Container();
             this.SplitContainer = new System.Windows.Forms.SplitContainer();
-            this.Attributes = new Reflexil.Editors.AttributesControl();
             this.RVA = new System.Windows.Forms.TextBox();
             this.LabRVA = new System.Windows.Forms.Label();
             this.LabCallingConvention = new System.Windows.Forms.Label();
             this.CallingConvention = new System.Windows.Forms.ComboBox();
+            this.GbxReturnType = new System.Windows.Forms.GroupBox();
+            this.ErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.Attributes = new Reflexil.Editors.AttributesControl();
+            this.ReturnType = new Reflexil.Editors.TypeSpecificationEditor();
             this.SplitContainer.Panel1.SuspendLayout();
             this.SplitContainer.Panel2.SuspendLayout();
             this.SplitContainer.SuspendLayout();
+            this.GbxReturnType.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorProvider)).BeginInit();
             this.SuspendLayout();
             // 
             // SplitContainer
@@ -52,6 +58,7 @@ namespace Reflexil.Editors
             // 
             // SplitContainer.Panel2
             // 
+            this.SplitContainer.Panel2.Controls.Add(this.GbxReturnType);
             this.SplitContainer.Panel2.Controls.Add(this.RVA);
             this.SplitContainer.Panel2.Controls.Add(this.LabRVA);
             this.SplitContainer.Panel2.Controls.Add(this.LabCallingConvention);
@@ -60,17 +67,9 @@ namespace Reflexil.Editors
             this.SplitContainer.SplitterDistance = 216;
             this.SplitContainer.TabIndex = 3;
             // 
-            // Attributes
-            // 
-            this.Attributes.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Attributes.Location = new System.Drawing.Point(0, 0);
-            this.Attributes.Name = "Attributes";
-            this.Attributes.Size = new System.Drawing.Size(216, 416);
-            this.Attributes.TabIndex = 0;
-            // 
             // RVA
             // 
-            this.RVA.Location = new System.Drawing.Point(107, 32);
+            this.RVA.Location = new System.Drawing.Point(92, 32);
             this.RVA.Name = "RVA";
             this.RVA.ReadOnly = true;
             this.RVA.Size = new System.Drawing.Size(100, 20);
@@ -79,7 +78,7 @@ namespace Reflexil.Editors
             // LabRVA
             // 
             this.LabRVA.AutoSize = true;
-            this.LabRVA.Location = new System.Drawing.Point(4, 35);
+            this.LabRVA.Location = new System.Drawing.Point(15, 35);
             this.LabRVA.Name = "LabRVA";
             this.LabRVA.Size = new System.Drawing.Size(29, 13);
             this.LabRVA.TabIndex = 3;
@@ -88,21 +87,57 @@ namespace Reflexil.Editors
             // LabCallingConvention
             // 
             this.LabCallingConvention.AutoSize = true;
-            this.LabCallingConvention.Location = new System.Drawing.Point(4, 7);
+            this.LabCallingConvention.Location = new System.Drawing.Point(15, 7);
             this.LabCallingConvention.Name = "LabCallingConvention";
-            this.LabCallingConvention.Size = new System.Drawing.Size(97, 13);
+            this.LabCallingConvention.Size = new System.Drawing.Size(61, 13);
             this.LabCallingConvention.TabIndex = 2;
-            this.LabCallingConvention.Text = "Calling convention:";
+            this.LabCallingConvention.Text = "Convention";
             // 
             // CallingConvention
             // 
             this.CallingConvention.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.CallingConvention.FormattingEnabled = true;
-            this.CallingConvention.Location = new System.Drawing.Point(107, 4);
+            this.CallingConvention.Location = new System.Drawing.Point(92, 4);
             this.CallingConvention.Name = "CallingConvention";
             this.CallingConvention.Size = new System.Drawing.Size(100, 21);
             this.CallingConvention.TabIndex = 1;
             this.CallingConvention.SelectionChangeCommitted += new System.EventHandler(this.CallingConvention_SelectionChangeCommitted);
+            // 
+            // GbxReturnType
+            // 
+            this.GbxReturnType.Controls.Add(this.ReturnType);
+            this.GbxReturnType.Location = new System.Drawing.Point(12, 72);
+            this.GbxReturnType.Name = "GbxReturnType";
+            this.GbxReturnType.Size = new System.Drawing.Size(408, 109);
+            this.GbxReturnType.TabIndex = 6;
+            this.GbxReturnType.TabStop = false;
+            this.GbxReturnType.Text = "Return type";
+            // 
+            // ErrorProvider
+            // 
+            this.ErrorProvider.ContainerControl = this;
+            // 
+            // Attributes
+            // 
+            this.Attributes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Attributes.Item = null;
+            this.Attributes.Location = new System.Drawing.Point(0, 0);
+            this.Attributes.Name = "Attributes";
+            this.Attributes.Size = new System.Drawing.Size(216, 416);
+            this.Attributes.TabIndex = 0;
+            // 
+            // ReturnType
+            // 
+            this.ReturnType.AllowArray = true;
+            this.ReturnType.AllowPointer = true;
+            this.ReturnType.AllowReference = false;
+            this.ReturnType.Location = new System.Drawing.Point(6, 19);
+            this.ReturnType.MethodDefinition = null;
+            this.ReturnType.Name = "ReturnType";
+            this.ReturnType.SelectedTypeReference = null;
+            this.ReturnType.Size = new System.Drawing.Size(383, 77);
+            this.ReturnType.TabIndex = 5;
+            this.ReturnType.Validating += new System.ComponentModel.CancelEventHandler(this.ReturnType_Validating);
             // 
             // MethodAttributesControl
             // 
@@ -115,6 +150,8 @@ namespace Reflexil.Editors
             this.SplitContainer.Panel2.ResumeLayout(false);
             this.SplitContainer.Panel2.PerformLayout();
             this.SplitContainer.ResumeLayout(false);
+            this.GbxReturnType.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorProvider)).EndInit();
             this.ResumeLayout(false);
 
 		}
@@ -127,5 +164,8 @@ namespace Reflexil.Editors
         private System.Windows.Forms.Label LabCallingConvention;
         private System.Windows.Forms.ComboBox CallingConvention;
         private Reflexil.Editors.AttributesControl Attributes;
+        private TypeSpecificationEditor ReturnType;
+        private System.Windows.Forms.GroupBox GbxReturnType;
+        private System.Windows.Forms.ErrorProvider ErrorProvider;
 	}
 }
