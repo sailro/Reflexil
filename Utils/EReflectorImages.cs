4420aef207aaf9cfdfd28e94c742c55355b4ea9e
sailro <sailro@245f54d4-4a38-0410-b06a-b1304f9c06f5>
2007-11-04 18:32:53 +0000

    Reflexil.Utils code comments
    git-svn-id: http://svn.code.sf.net/p/reflexil/code@72 245f54d4-4a38-0410-b06a-b1304f9c06f5

--- Utils/EReflectorImages.cs
+++ Utils/EReflectorImages.cs
@@ -1,5 +1,26 @@
+/*
+    Reflexil .NET assembly editor.
+    Copyright (C) 2007 Sebastien LEBRETON
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 namespace Reflexil.Utils
 {
+    /// <summary>
+    /// Reflector images used in the browser
+    /// </summary>
 	public enum EReflectorImages
 	{
 		Empty,
@@ -138,7 +159,6 @@ namespace Reflexil.Utils
 		PrivateSharedProperty,
 		LinkedSharedProperty,
 		
-		
 		PublicReadOnlyProperty,
 		FriendReadOnlyProperty,
 		ProtectedFriendReadOnlyProperty,
@@ -188,6 +208,5 @@ namespace Reflexil.Utils
 		References,
 		Resources
 	}
-	
 }
 
