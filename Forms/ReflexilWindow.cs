9ec8738357a59fc3e49ba232c098bcdd74894e1d
sailro <sailro@245f54d4-4a38-0410-b06a-b1304f9c06f5>
2007-12-17 16:19:38 +0000

    Type attributes editor
    git-svn-id: http://svn.code.sf.net/p/reflexil/code@78 245f54d4-4a38-0410-b06a-b1304f9c06f5

--- Forms/ReflexilWindow.cs
+++ Forms/ReflexilWindow.cs
@@ -11,7 +11,9 @@ using Reflexil.Handlers;
 
 namespace Reflexil.Forms
 {
-	
+	/// <summary>
+	/// Main reflexil window
+	/// </summary>
 	public partial class ReflexilWindow
 	{
 		
@@ -20,15 +22,23 @@ namespace Reflexil.Forms
 		#endregion
 		
 		#region " Methods "
+        /// <summary>
+        /// Constructor
+        /// </summary>
 		public ReflexilWindow() : base()
 		{
 			InitializeComponent();
 			
 			m_handlers.Add(new ModuleDefinitionHandler());
+            m_handlers.Add(new TypeDefinitionHandler());
 			m_handlers.Add(new MethodDefinitionHandler());
 			m_handlers.Add(new NotSupportedHandler());
 		}
 		
+        /// <summary>
+        /// Handle reflector tree item
+        /// </summary>
+        /// <param name="item">Item to handle</param>
 		public void HandleItem(object item)
 		{
 			TabControl.TabPages.Clear();
@@ -51,6 +61,11 @@ namespace Reflexil.Forms
 			}
 		}
 
+        /// <summary>
+        /// Handle configure button click 
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">attributes</param>
         private void Configure_Click(object sender, EventArgs e)
         {
             using (ConfigureForm frm = new ConfigureForm())
