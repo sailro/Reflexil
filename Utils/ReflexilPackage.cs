a81ff08f492270ee340333c4455f7da11db569ee
Sebastien LEBRETON <sebastien@lebreton.fr>
2008-05-25 19:24:38 +0000

    Preparing next Preparing next version
    git-svn-id: http://svn.code.sf.net/p/reflexil/code@110 245f54d4-4a38-0410-b06a-b1304f9c06f5

--- Utils/ReflexilPackage.cs
+++ Utils/ReflexilPackage.cs
@@ -20,8 +20,12 @@
 using System;
 using Reflector;
 using Reflector.CodeModel;
+using Reflector.CodeModel.Memory;
 using Reflexil.Forms;
 using Reflexil.Properties;
+using Mono.Cecil;
+using System.Windows.Forms;
+using Reflexil.Handlers;
 #endregion
 
 namespace Reflexil.Utils
@@ -34,10 +38,12 @@ namespace Reflexil.Utils
 		
 		#region " Constants "
 		const string REFLECTOR_TOOLS_ID = "Tools";
+        //const string REFLECTOR_TYPEDEC_ID = "Browser.TypeDeclaration";
 		readonly string REFLEXIL_WINDOW_TEXT = string.Format("Sebastien LEBRETON's Reflexil v{0}", typeof(ReflexilPackage).Assembly.GetName().Version.ToString(2));
         readonly string REFLEXIL_BUTTON_TEXT = string.Format("Reflexil v{0}", typeof(ReflexilPackage).Assembly.GetName().Version.ToString(2));
         const string REFLEXIL_WINDOW_ID = "Reflexil.Window";
-		const string REFLECTOR_RESOURCE_OPCODES = "Reflector.Disassembler.txt";
+        //const string REFLEXIL_TYPEDEC_MENU_ID = "Reflexil.TypeDeclaration.Menu";
+        const string REFLECTOR_RESOURCE_OPCODES = "Reflector.Disassembler.txt";
 		const string REFLECTOR_RESOURCE_IMAGES = "Reflector.Browser16.png";
 		#endregion
 		
@@ -48,12 +54,75 @@ namespace Reflexil.Utils
 		private IAssemblyManager am;
 		private IServiceProvider sp;
 		
-		private ICommandBarButton button;
-		private ICommandBarSeparator separator;
+		private ICommandBarButton mainMenuButton;
+        private ICommandBarMenu typeMenu;
+        private ICommandBarSeparator typeMenuSeparator;
+        private ICommandBarSeparator mainMenuSeparator;
 		private ReflexilWindow window;
+        private IHandler activehandler;
 		#endregion
 		
 		#region " Events "
+        // Next version
+        // enum must match TypeDefinitionHandler methods names
+        //private enum TypeAction { AddConstructor, AddField, AddMethod, AddProperty, AddEvent};
+        //private void HandleTypeAction(TypeAction action)
+        //{
+        //    if (ab.ActiveItem is ITypeDeclaration && activehandler is TypeDefinitionHandler)
+        //    {
+        //        ITypeDeclaration activeitem = ab.ActiveItem as ITypeDeclaration;
+
+        //        // Retrieve infos
+        //        string tempfile = System.IO.Path.GetTempFileName();
+        //        IModule module = CecilHelper.GetModule(activeitem);
+        //        IAssembly refasm = module.Assembly;
+        //        AssemblyDefinition asmdef = DataManager.GetInstance().GetAssemblyContext(refasm.Location).AssemblyDefinition;
+
+        //        // Update assembly
+        //        typeof(TypeDefinitionHandler).GetMethod(action.ToString()).Invoke(activehandler, null);
+                
+        //        // Save assembly then unload
+        //        AssemblyFactory.SaveAssembly(asmdef, tempfile);
+        //        DataManager.GetInstance().RemoveAssemblyContext(refasm.Location);
+        //        am.Unload(refasm);
+
+        //        // Reload
+        //        refasm = am.LoadFile(tempfile);
+        //        asmdef = DataManager.GetInstance().GetAssemblyContext(refasm.Location).AssemblyDefinition;
+
+        //        activeitem = CecilHelper.FindMatchingType(refasm, activeitem);
+        //        if (activeitem != null)
+        //        {
+        //            ab.ActiveItem = activeitem;
+        //        }
+        //    }
+        //}
+        
+        //private void AddConstructor(object sender, EventArgs e)
+        //{
+        //    HandleTypeAction(TypeAction.AddConstructor);
+        //}
+
+        //private void AddField(object sender, EventArgs e)
+        //{
+        //    HandleTypeAction(TypeAction.AddField);
+        //}
+
+        //private void AddMethod(object sender, EventArgs e)
+        //{
+        //    HandleTypeAction(TypeAction.AddMethod);
+        //}
+
+        //private void AddProperty(object sender, EventArgs e)
+        //{
+        //    HandleTypeAction(TypeAction.AddProperty);
+        //}
+
+        //private void AddEvent(object sender, EventArgs e)
+        //{
+        //    HandleTypeAction(TypeAction.AddEvent);
+        //}
+
         /// <summary>
         /// 'Reflexil' button click 
         /// </summary>
@@ -71,7 +140,7 @@ namespace Reflexil.Utils
         /// <param name="e">Event parameters</param>
         private void ab_ActiveItemChanged(object sender, EventArgs e)
 		{
-			window.HandleItem(ab.ActiveItem);
+            activehandler = window.HandleItem(ab.ActiveItem);
 		}
 		
         /// <summary>
@@ -120,17 +189,34 @@ namespace Reflexil.Utils
 			am = GetService<IAssemblyManager>();
 			sp = GetService<IServiceProvider>();
 
+            // Main Window
 			window = new Reflexil.Forms.ReflexilWindow();
 			wm.Windows.Add(REFLEXIL_WINDOW_ID, window, REFLEXIL_WINDOW_TEXT);
 			
-			separator = cbm.CommandBars[REFLECTOR_TOOLS_ID].Items.AddSeparator();
-			button = cbm.CommandBars[REFLECTOR_TOOLS_ID].Items.AddButton(REFLEXIL_BUTTON_TEXT, null);
-			button.Image = Resources.reflexil;
+            // Menu
+			mainMenuSeparator = cbm.CommandBars[REFLECTOR_TOOLS_ID].Items.AddSeparator();
+			mainMenuButton = cbm.CommandBars[REFLECTOR_TOOLS_ID].Items.AddButton(REFLEXIL_BUTTON_TEXT, null);
+			mainMenuButton.Image = Resources.reflexil;
 
+            // Type declaration menu
+            //typeMenuSeparator = cbm.CommandBars[REFLECTOR_TYPEDEC_ID].Items.AddSeparator();
+            //typeMenu = cbm.CommandBars[REFLECTOR_TYPEDEC_ID].Items.AddMenu(REFLEXIL_TYPEDEC_MENU_ID, REFLEXIL_BUTTON_TEXT);
+            //typeMenu.Image = Resources.reflexil;
+            //typeMenu.Items.AddButton("Add constructor", AddConstructor);
+            //typeMenu.Items.AddButton("Add event", AddEvent);
+            //typeMenu.Items.AddButton("Add field", AddField);
+            //typeMenu.Items.AddButton("Add method", AddMethod);
+            //typeMenu.Items.AddButton("Add property", AddProperty);
+
+            // Constructor declaration menu
+
+            // Main events
             ab.ActiveItemChanged += this.ab_ActiveItemChanged;
             am.AssemblyLoaded += this.am_AssemblyLoaded;
             am.AssemblyUnloaded += this.am_AssemblyUnloaded;
-            button.Click += this.button_Click;
+            
+            // Menu events
+            mainMenuButton.Click += this.button_Click;
 			
 			System.Reflection.Assembly reflectorAssembly = ((object) serviceProvider).GetType().Assembly;
             DataManager.GetInstance().ReloadOpcodesDesc(reflectorAssembly.GetManifestResourceStream(REFLECTOR_RESOURCE_OPCODES));
@@ -145,15 +231,25 @@ namespace Reflexil.Utils
         /// </summary>
 		public void Unload()
 		{
+            // Main events
             ab.ActiveItemChanged -= this.ab_ActiveItemChanged;
             am.AssemblyLoaded -= this.am_AssemblyLoaded;
             am.AssemblyUnloaded -= this.am_AssemblyUnloaded;
-            button.Click -= this.button_Click;
 
-			wm.Windows.Remove(REFLEXIL_WINDOW_ID);
-			cbm.CommandBars[REFLECTOR_TOOLS_ID].Items.Remove(button);
-			cbm.CommandBars[REFLECTOR_TOOLS_ID].Items.Remove(separator);
-		}
+            // Menu events
+            mainMenuButton.Click -= this.button_Click;
+
+            // Main Window
+            wm.Windows.Remove(REFLEXIL_WINDOW_ID);
+
+            // Menu
+            cbm.CommandBars[REFLECTOR_TOOLS_ID].Items.Remove(mainMenuButton);
+			cbm.CommandBars[REFLECTOR_TOOLS_ID].Items.Remove(mainMenuSeparator);
+
+            // Type declaration menu
+            //cbm.CommandBars[REFLECTOR_TYPEDEC_ID].Items.Remove(typeMenu);
+            //cbm.CommandBars[REFLECTOR_TYPEDEC_ID].Items.Remove(typeMenuSeparator);
+        }
 		
 		#endregion
 		
