d9d57ee01f56d3c1153d938d0e9c7a872f704d2c
Sebastien LEBRETON <sebastien@lebreton.fr>
2009-04-30 16:29:34 +0000

    v0.9.1
    git-svn-id: http://svn.code.sf.net/p/reflexil/code@135 245f54d4-4a38-0410-b06a-b1304f9c06f5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // <Assembly: AssemblyVersion("1.0.*")>
 
 [assembly: AssemblyVersion("0.9.*")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.1.0")]
 
