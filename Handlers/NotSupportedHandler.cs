23ba982eac8a7c22740d5fc403219e07ec2eaa88
Sebastien LEBRETON <sebastien@lebreton.fr>
2009-11-15 10:18:05 +0000

    Headers updated and some resources added
    git-svn-id: http://svn.code.sf.net/p/reflexil/code@147 245f54d4-4a38-0410-b06a-b1304f9c06f5

--- Handlers/NotSupportedHandler.cs
+++ Handlers/NotSupportedHandler.cs
@@ -1,6 +1,6 @@
 /*
     Reflexil .NET assembly editor.
-    Copyright (C) 2007 Sebastien LEBRETON
+    Copyright (C) 2007-2009 Sebastien LEBRETON
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
