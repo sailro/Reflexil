f1d9ea0647f2b549fe5a37f1c8ec99ab78318204
Sebastien LEBRETON <sebastien@lebreton.fr>
2010-04-22 06:58:55 +0000

    Custom automated build
    git-svn-id: http://svn.code.sf.net/p/reflexil/code@177 245f54d4-4a38-0410-b06a-b1304f9c06f5

--- Plugins/Reflexil.Reflector/Properties/AssemblyInfo.cs
+++ Plugins/Reflexil.Reflector/Properties/AssemblyInfo.cs
@@ -9,8 +9,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2007-2010 Sebastien LEBRETON")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: ComVisible(false)]
 [assembly: Guid("da360a2f-3e7f-47be-b3cf-1bc7e6669721")]
-
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
