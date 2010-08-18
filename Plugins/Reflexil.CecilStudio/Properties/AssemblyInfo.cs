fa09c1a85e99c638fb589f3010f4f4fc6436761f
Sebastien LEBRETON <sebastien@lebreton.fr>
2010-08-18 13:28:05 +0000

    Removing AutoBuild dependencies
    git-svn-id: http://svn.code.sf.net/p/reflexil/code@186 245f54d4-4a38-0410-b06a-b1304f9c06f5

--- Plugins/Reflexil.CecilStudio/Properties/AssemblyInfo.cs
+++ Plugins/Reflexil.CecilStudio/Properties/AssemblyInfo.cs
@@ -4,9 +4,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Reflexil.CecilStudio")]
 [assembly: AssemblyDescription("Reflexil plugin for Cecil Studio")]
-[assembly: AssemblyCompany("mailto:reflexil@free.fr")]
-[assembly: AssemblyProduct("Reflexil")]
-[assembly: AssemblyCopyright("Copyright © 2007-2010 Sebastien LEBRETON")]
-[assembly: AssemblyTrademark("")]
 
 [assembly: Guid("408c1196-a70d-4c1d-a3f1-3896148a3548")]
