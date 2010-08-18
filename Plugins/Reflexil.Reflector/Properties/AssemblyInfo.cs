7013e3e7c0d52b71962e6b79bdcffb6bbfe3ed39
sailro <sailro@245f54d4-4a38-0410-b06a-b1304f9c06f5>
2010-08-18 13:28:05 +0000

    Removing AutoBuild dependencies
    git-svn-id: http://svn.code.sf.net/p/reflexil/code@186 245f54d4-4a38-0410-b06a-b1304f9c06f5

--- Plugins/Reflexil.Reflector/Properties/AssemblyInfo.cs
+++ Plugins/Reflexil.Reflector/Properties/AssemblyInfo.cs
@@ -4,9 +4,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Reflexil.Reflector")]
 [assembly: AssemblyDescription("Reflexil plugin for Reflector")]
-[assembly: AssemblyCompany("mailto:reflexil@free.fr")]
-[assembly: AssemblyProduct("Reflexil")]
-[assembly: AssemblyCopyright("Copyright © 2007-2010 Sebastien LEBRETON")]
-[assembly: AssemblyTrademark("")]
 
 [assembly: Guid("da360a2f-3e7f-47be-b3cf-1bc7e6669721")]
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,9 +4,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Reflexil")]
 [assembly: AssemblyDescription("Reflexil main module")]
-[assembly: AssemblyCompany("mailto:reflexil@free.fr")]
-[assembly: AssemblyProduct("Reflexil")]
-[assembly: AssemblyCopyright("Copyright © 2007-2010 Sebastien LEBRETON")]
-[assembly: AssemblyTrademark("")]
 
 [assembly: Guid("ec49f9ed-2efb-41df-83c8-d7d53b3d777a")]
