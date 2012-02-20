278e671026177a1d9699dce8ec02b32f6e5666d6
Sebastien LEBRETON <sebastien@lebreton.fr>
2012-02-20 15:45:11 +0000

    Auto packager for binary release
    git-svn-id: http://svn.code.sf.net/p/reflexil/code@246 245f54d4-4a38-0410-b06a-b1304f9c06f5

---
+++ Build/Binary/generate.bat
@@ -0,0 +1,30 @@
+@echo off
+
+set PATH=.\tools;%WINDIR%\Microsoft.NET\Framework\v4.0.30319;%PATH%
+
+msbuild ..\..\reflexil.sln /p:Configuration=Release;TargetFrameworkVersion=v3.5
+
+rm -fr output
+mkdir output
+
+cp ..\..\bin\Release\*.dll output
+cp ..\..\bin\Release\*.config output
+cp ..\..\Plugins\Reflexil.Reflector\bin\Release\*.dll output
+cp ..\..\Plugins\Reflexil.CecilStudio\bin\Release\*.dll output
+
+cp ..\..\Changelog output
+cp ..\..\MIT-LICENSE.txt output
+cp ..\..\credits.txt output
+cp ..\..\README.FIRST.txt output
+
+grep "AssemblyVersion" ..\..\Properties\SolutionVersion.cs | sed "s/.*\"\(.\..\)\..\..\".*/\1/g" > version.txt
+set /P REFLEXIL_VERSION=<version.txt
+
+rm -fr reflexil.%REFLEXIL_VERSION%.bin
+mv output reflexil.%REFLEXIL_VERSION%.bin
+
+rm reflexil.%REFLEXIL_VERSION%.bin.zip
+zip -r reflexil.%REFLEXIL_VERSION%.bin.zip reflexil.%REFLEXIL_VERSION%.bin
+
+rm -fr reflexil.%REFLEXIL_VERSION%.bin
+rm version.txt
