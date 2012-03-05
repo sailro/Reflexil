d486aae2780b06e1fae54e8dad48751a0fac9621
Sebastien LEBRETON <sebastien@lebreton.fr>
2012-03-05 21:21:12 +0000

    Cleaning
    git-svn-id: http://svn.code.sf.net/p/reflexil/code@255 245f54d4-4a38-0410-b06a-b1304f9c06f5

--- Libs/Binaries/De4dot/4-merge.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set PATH=.\tools;%PATH%
-
-ILMerge /keyfile:.\sources\de4dot.snk /out:De4Dot.dll .\sources\Release\de4dot.code.dll .\sources\Release\blocks.dll .\sources\Release\AssemblyData.dll .\sources\Release\ICSharpCode.SharpZipLib.dll .\sources\Release\Mono.Cecil.dll
-
-mv -f De4Dot.dll ..\
-rm De4Dot.pdb
