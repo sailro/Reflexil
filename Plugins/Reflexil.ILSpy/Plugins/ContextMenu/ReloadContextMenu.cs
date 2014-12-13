468b9e979ee4d4c2661bc0c4a79093f39f401175
Sebastien LEBRETON <sebastien@lebreton.fr>
2014-12-13 21:40:53 +0100

    Typo

--- Plugins/Reflexil.ILSpy/Plugins/ContextMenu/ReloadContextMenu.cs
+++ Plugins/Reflexil.ILSpy/Plugins/ContextMenu/ReloadContextMenu.cs
@@ -4,7 +4,7 @@ using ICSharpCode.TreeView;
 
 namespace Reflexil.Plugins.ILSpy.ContextMenu
 {
-	[ExportContextMenuEntry(Icon = "resources/reload.png", Header = "Reload", Category = "ReflexilMain")]
+	[ExportContextMenuEntry(Icon = "resources/reload.png", Header = "Reload Reflexil codemodel", Category = "ReflexilMain")]
 	internal class ReloadContextMenu : BaseAssemblyOrModuleContextMenu
     {
 		protected override void Execute(SharpTreeNode node)
