b59d4aada61049b2b38e126e2fe1299600cdd10e
Sebastien Lebreton <sebastien@lebreton.fr>
2015-03-19 15:37:17 +0100

    ChangeLog and Copyrights

--- Controls/Reference/TypeReferenceEditor.cs
+++ Controls/Reference/TypeReferenceEditor.cs
@@ -1,4 +1,4 @@
-/* Reflexil Copyright (c) 2007-2014 Sebastien LEBRETON
+/* Reflexil Copyright (c) 2007-2015 Sebastien LEBRETON
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
