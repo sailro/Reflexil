7edecb08ae0229f576de36140ad119e8e8e2e1ef
Sebastien Lebreton <sebastien@lebreton.fr>
2015-08-26 11:23:02 +0200

    Update download script to correctly follow redirects

--- Libs/Sources/De4dot/1-download.bat
+++ Libs/Sources/De4dot/1-download.bat
@@ -3,8 +3,8 @@
 set PATH=.\tools;%PATH%
 
 rm -fr sources
-wget https://github.com/0xd4d/de4dot/archive/master.zip -O de4dot.zip
-wget https://github.com/0xd4d/dnlib/archive/master.zip -O dnlib.zip
+curl -L -k -o de4dot.zip https://github.com/0xd4d/de4dot/archive/master.zip
+curl -L -k -o dnlib.zip https://github.com/0xd4d/dnlib/archive/master.zip
 unzip de4dot.zip
 unzip dnlib.zip
 mv dnlib-master dnlib
