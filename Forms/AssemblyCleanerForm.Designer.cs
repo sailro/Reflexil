c235491902f29d205bd65e5e44df1d13b227040b
Sebastien Lebreton <sebleb@microsoft.com>
2018-08-17 14:27:45 -0700

    Fix DPI issues with progress bars

--- Forms/AssemblyCleanerForm.Designer.cs
+++ Forms/AssemblyCleanerForm.Designer.cs
@@ -28,96 +28,103 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.BackgroundWorker = new System.ComponentModel.BackgroundWorker();
-            this.Cancel = new System.Windows.Forms.Button();
-            this.StepLab = new System.Windows.Forms.Label();
-            this.ProgressBar = new System.Windows.Forms.ProgressBar();
-            this.Directory = new System.Windows.Forms.Label();
-            this.Step = new System.Windows.Forms.Label();
-            this.WaitLab = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // BackgroundWorker
-            // 
-            this.BackgroundWorker.WorkerReportsProgress = true;
-            this.BackgroundWorker.WorkerSupportsCancellation = true;
-            this.BackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker_DoWork);
-            this.BackgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BackgroundWorker_ProgressChanged);
-            this.BackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker_RunWorkerCompleted);
-            // 
-            // Cancel
-            // 
-            this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.Cancel.Location = new System.Drawing.Point(379, 91);
-            this.Cancel.Name = "Cancel";
-            this.Cancel.Size = new System.Drawing.Size(75, 23);
-            this.Cancel.TabIndex = 0;
-            this.Cancel.Text = "Cancel";
-            this.Cancel.UseVisualStyleBackColor = true;
-            this.Cancel.Visible = false;
-            // 
-            // StepLab
-            // 
-            this.StepLab.AutoSize = true;
-            this.StepLab.Location = new System.Drawing.Point(13, 13);
-            this.StepLab.Name = "StepLab";
-            this.StepLab.Size = new System.Drawing.Size(32, 13);
-            this.StepLab.TabIndex = 2;
-            this.StepLab.Text = "Step:";
-            // 
-            // ProgressBar
-            // 
-            this.ProgressBar.Location = new System.Drawing.Point(16, 59);
-            this.ProgressBar.Name = "ProgressBar";
-            this.ProgressBar.Size = new System.Drawing.Size(438, 23);
-            this.ProgressBar.TabIndex = 3;
-            // 
-            // Directory
-            // 
-            this.Directory.Location = new System.Drawing.Point(71, 13);
-            this.Directory.Name = "Directory";
-            this.Directory.Size = new System.Drawing.Size(383, 23);
-            this.Directory.TabIndex = 4;
-            // 
-            // Step
-            // 
-            this.Step.Location = new System.Drawing.Point(71, 13);
-            this.Step.Name = "Step";
-            this.Step.Size = new System.Drawing.Size(383, 43);
-            this.Step.TabIndex = 5;
-            // 
-            // WaitLab
-            // 
-            this.WaitLab.AutoSize = true;
-            this.WaitLab.Location = new System.Drawing.Point(16, 97);
-            this.WaitLab.Name = "WaitLab";
-            this.WaitLab.Size = new System.Drawing.Size(186, 13);
-            this.WaitLab.TabIndex = 6;
-            this.WaitLab.Text = "Please wait while cleaning assembly...";
-            // 
-            // AssemblyCleanerForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.Cancel;
-            this.ClientSize = new System.Drawing.Size(466, 123);
-            this.ControlBox = false;
-            this.Controls.Add(this.WaitLab);
-            this.Controls.Add(this.Step);
-            this.Controls.Add(this.Directory);
-            this.Controls.Add(this.ProgressBar);
-            this.Controls.Add(this.StepLab);
-            this.Controls.Add(this.Cancel);
-            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "AssemblyCleanerForm";
-            this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Assembly cleaner";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.BackgroundWorker = new System.ComponentModel.BackgroundWorker();
+			this.Cancel = new System.Windows.Forms.Button();
+			this.StepLab = new System.Windows.Forms.Label();
+			this.ProgressBar = new System.Windows.Forms.ProgressBar();
+			this.Directory = new System.Windows.Forms.Label();
+			this.Step = new System.Windows.Forms.Label();
+			this.WaitLab = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// BackgroundWorker
+			// 
+			this.BackgroundWorker.WorkerReportsProgress = true;
+			this.BackgroundWorker.WorkerSupportsCancellation = true;
+			this.BackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker_DoWork);
+			this.BackgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BackgroundWorker_ProgressChanged);
+			this.BackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker_RunWorkerCompleted);
+			// 
+			// Cancel
+			// 
+			this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.Cancel.Location = new System.Drawing.Point(505, 112);
+			this.Cancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Cancel.Name = "Cancel";
+			this.Cancel.Size = new System.Drawing.Size(100, 28);
+			this.Cancel.TabIndex = 0;
+			this.Cancel.Text = "Cancel";
+			this.Cancel.UseVisualStyleBackColor = true;
+			this.Cancel.Visible = false;
+			// 
+			// StepLab
+			// 
+			this.StepLab.AutoSize = true;
+			this.StepLab.Location = new System.Drawing.Point(17, 16);
+			this.StepLab.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.StepLab.Name = "StepLab";
+			this.StepLab.Size = new System.Drawing.Size(41, 17);
+			this.StepLab.TabIndex = 2;
+			this.StepLab.Text = "Step:";
+			// 
+			// ProgressBar
+			// 
+			this.ProgressBar.Location = new System.Drawing.Point(21, 73);
+			this.ProgressBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.ProgressBar.Name = "ProgressBar";
+			this.ProgressBar.Size = new System.Drawing.Size(584, 28);
+			this.ProgressBar.TabIndex = 3;
+			// 
+			// Directory
+			// 
+			this.Directory.Location = new System.Drawing.Point(95, 16);
+			this.Directory.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.Directory.Name = "Directory";
+			this.Directory.Size = new System.Drawing.Size(511, 28);
+			this.Directory.TabIndex = 4;
+			// 
+			// Step
+			// 
+			this.Step.Location = new System.Drawing.Point(95, 16);
+			this.Step.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.Step.Name = "Step";
+			this.Step.Size = new System.Drawing.Size(511, 53);
+			this.Step.TabIndex = 5;
+			// 
+			// WaitLab
+			// 
+			this.WaitLab.AutoSize = true;
+			this.WaitLab.Location = new System.Drawing.Point(21, 119);
+			this.WaitLab.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.WaitLab.Name = "WaitLab";
+			this.WaitLab.Size = new System.Drawing.Size(246, 17);
+			this.WaitLab.TabIndex = 6;
+			this.WaitLab.Text = "Please wait while cleaning assembly...";
+			// 
+			// AssemblyCleanerForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.CancelButton = this.Cancel;
+			this.ClientSize = new System.Drawing.Size(621, 151);
+			this.ControlBox = false;
+			this.Controls.Add(this.WaitLab);
+			this.Controls.Add(this.Step);
+			this.Controls.Add(this.Directory);
+			this.Controls.Add(this.ProgressBar);
+			this.Controls.Add(this.StepLab);
+			this.Controls.Add(this.Cancel);
+			this.Cursor = System.Windows.Forms.Cursors.AppStarting;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "AssemblyCleanerForm";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Assembly cleaner";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
--- Forms/DirectoryScanForm.Designer.cs
+++ Forms/DirectoryScanForm.Designer.cs
@@ -28,106 +28,114 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.BackgroundWorker = new System.ComponentModel.BackgroundWorker();
-            this.Cancel = new System.Windows.Forms.Button();
-            this.DirectoryLab = new System.Windows.Forms.Label();
-            this.FileLab = new System.Windows.Forms.Label();
-            this.ProgressBar = new System.Windows.Forms.ProgressBar();
-            this.Directory = new System.Windows.Forms.Label();
-            this.File = new System.Windows.Forms.Label();
-            this.WaitLab = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // BackgroundWorker
-            // 
-            this.BackgroundWorker.WorkerReportsProgress = true;
-            this.BackgroundWorker.WorkerSupportsCancellation = true;
-            this.BackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker_DoWork);
-            this.BackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker_RunWorkerCompleted);
-            this.BackgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BackgroundWorker_ProgressChanged);
-            // 
-            // Cancel
-            // 
-            this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.Cancel.Location = new System.Drawing.Point(379, 114);
-            this.Cancel.Name = "Cancel";
-            this.Cancel.Size = new System.Drawing.Size(75, 23);
-            this.Cancel.TabIndex = 0;
-            this.Cancel.Text = "Cancel";
-            this.Cancel.UseVisualStyleBackColor = true;
-            // 
-            // DirectoryLab
-            // 
-            this.DirectoryLab.AutoSize = true;
-            this.DirectoryLab.Location = new System.Drawing.Point(13, 13);
-            this.DirectoryLab.Name = "DirectoryLab";
-            this.DirectoryLab.Size = new System.Drawing.Size(52, 13);
-            this.DirectoryLab.TabIndex = 1;
-            this.DirectoryLab.Text = "Directory:";
-            // 
-            // FileLab
-            // 
-            this.FileLab.AutoSize = true;
-            this.FileLab.Location = new System.Drawing.Point(13, 36);
-            this.FileLab.Name = "FileLab";
-            this.FileLab.Size = new System.Drawing.Size(26, 13);
-            this.FileLab.TabIndex = 2;
-            this.FileLab.Text = "File:";
-            // 
-            // ProgressBar
-            // 
-            this.ProgressBar.Location = new System.Drawing.Point(16, 82);
-            this.ProgressBar.Name = "ProgressBar";
-            this.ProgressBar.Size = new System.Drawing.Size(438, 23);
-            this.ProgressBar.TabIndex = 3;
-            // 
-            // Directory
-            // 
-            this.Directory.Location = new System.Drawing.Point(71, 13);
-            this.Directory.Name = "Directory";
-            this.Directory.Size = new System.Drawing.Size(383, 23);
-            this.Directory.TabIndex = 4;
-            // 
-            // File
-            // 
-            this.File.Location = new System.Drawing.Point(71, 36);
-            this.File.Name = "File";
-            this.File.Size = new System.Drawing.Size(383, 43);
-            this.File.TabIndex = 5;
-            // 
-            // WaitLab
-            // 
-            this.WaitLab.AutoSize = true;
-            this.WaitLab.Location = new System.Drawing.Point(16, 120);
-            this.WaitLab.Name = "WaitLab";
-            this.WaitLab.Size = new System.Drawing.Size(311, 13);
-            this.WaitLab.TabIndex = 6;
-            this.WaitLab.Text = "Please wait while scanning directory for referencing assemblies...";
-            // 
-            // DirectoryScanForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.Cancel;
-            this.ClientSize = new System.Drawing.Size(466, 151);
-            this.ControlBox = false;
-            this.Controls.Add(this.WaitLab);
-            this.Controls.Add(this.File);
-            this.Controls.Add(this.Directory);
-            this.Controls.Add(this.ProgressBar);
-            this.Controls.Add(this.FileLab);
-            this.Controls.Add(this.DirectoryLab);
-            this.Controls.Add(this.Cancel);
-            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "DirectoryScanForm";
-            this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Directory Scanner";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.BackgroundWorker = new System.ComponentModel.BackgroundWorker();
+			this.Cancel = new System.Windows.Forms.Button();
+			this.DirectoryLab = new System.Windows.Forms.Label();
+			this.FileLab = new System.Windows.Forms.Label();
+			this.ProgressBar = new System.Windows.Forms.ProgressBar();
+			this.Directory = new System.Windows.Forms.Label();
+			this.File = new System.Windows.Forms.Label();
+			this.WaitLab = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// BackgroundWorker
+			// 
+			this.BackgroundWorker.WorkerReportsProgress = true;
+			this.BackgroundWorker.WorkerSupportsCancellation = true;
+			this.BackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker_DoWork);
+			this.BackgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BackgroundWorker_ProgressChanged);
+			this.BackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker_RunWorkerCompleted);
+			// 
+			// Cancel
+			// 
+			this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.Cancel.Location = new System.Drawing.Point(505, 140);
+			this.Cancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Cancel.Name = "Cancel";
+			this.Cancel.Size = new System.Drawing.Size(100, 28);
+			this.Cancel.TabIndex = 0;
+			this.Cancel.Text = "Cancel";
+			this.Cancel.UseVisualStyleBackColor = true;
+			// 
+			// DirectoryLab
+			// 
+			this.DirectoryLab.AutoSize = true;
+			this.DirectoryLab.Location = new System.Drawing.Point(17, 16);
+			this.DirectoryLab.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.DirectoryLab.Name = "DirectoryLab";
+			this.DirectoryLab.Size = new System.Drawing.Size(69, 17);
+			this.DirectoryLab.TabIndex = 1;
+			this.DirectoryLab.Text = "Directory:";
+			// 
+			// FileLab
+			// 
+			this.FileLab.AutoSize = true;
+			this.FileLab.Location = new System.Drawing.Point(17, 44);
+			this.FileLab.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.FileLab.Name = "FileLab";
+			this.FileLab.Size = new System.Drawing.Size(34, 17);
+			this.FileLab.TabIndex = 2;
+			this.FileLab.Text = "File:";
+			// 
+			// ProgressBar
+			// 
+			this.ProgressBar.Location = new System.Drawing.Point(21, 101);
+			this.ProgressBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.ProgressBar.Name = "ProgressBar";
+			this.ProgressBar.Size = new System.Drawing.Size(584, 28);
+			this.ProgressBar.TabIndex = 3;
+			// 
+			// Directory
+			// 
+			this.Directory.Location = new System.Drawing.Point(95, 16);
+			this.Directory.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.Directory.Name = "Directory";
+			this.Directory.Size = new System.Drawing.Size(511, 28);
+			this.Directory.TabIndex = 4;
+			// 
+			// File
+			// 
+			this.File.Location = new System.Drawing.Point(95, 44);
+			this.File.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.File.Name = "File";
+			this.File.Size = new System.Drawing.Size(511, 53);
+			this.File.TabIndex = 5;
+			// 
+			// WaitLab
+			// 
+			this.WaitLab.AutoSize = true;
+			this.WaitLab.Location = new System.Drawing.Point(21, 148);
+			this.WaitLab.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.WaitLab.Name = "WaitLab";
+			this.WaitLab.Size = new System.Drawing.Size(417, 17);
+			this.WaitLab.TabIndex = 6;
+			this.WaitLab.Text = "Please wait while scanning directory for referencing assemblies...";
+			// 
+			// DirectoryScanForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.CancelButton = this.Cancel;
+			this.ClientSize = new System.Drawing.Size(621, 186);
+			this.ControlBox = false;
+			this.Controls.Add(this.WaitLab);
+			this.Controls.Add(this.File);
+			this.Controls.Add(this.Directory);
+			this.Controls.Add(this.ProgressBar);
+			this.Controls.Add(this.FileLab);
+			this.Controls.Add(this.DirectoryLab);
+			this.Controls.Add(this.Cancel);
+			this.Cursor = System.Windows.Forms.Cursors.AppStarting;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "DirectoryScanForm";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Directory Scanner";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
