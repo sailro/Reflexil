33198fa948ac4da6c627bda46468f2fd407fbdd4
Sebastien Lebreton <sebastien@lebreton.fr>
2020-07-15 19:48:03 -0700

    Copyright update

--- Properties/SolutionVersion.cs
+++ Properties/SolutionVersion.cs
@@ -5,5 +5,5 @@ using System.Reflection;
 [assembly: AssemblyInformationalVersionAttribute("2.4.0.0")]
 [assembly: AssemblyCompany("mailto:contact@reflexil.net")]
 [assembly: AssemblyProduct("Reflexil")]
-[assembly: AssemblyCopyright("Copyright © 2007-2019 Sebastien Lebreton")]
+[assembly: AssemblyCopyright("Copyright © 2007-2020 Sebastien Lebreton")]
 [assembly: AssemblyTrademark("")]
