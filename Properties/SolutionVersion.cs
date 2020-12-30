d27f4f089757d4c5d6a5a9ca9ad076966e3f6e36
Sebastien Lebreton <sebastien@lebreton.fr>
2020-12-29 16:32:13 -0800

    Bump to 2.5

--- Properties/SolutionVersion.cs
+++ Properties/SolutionVersion.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 
-[assembly: AssemblyVersionAttribute("2.4.0.0")]
-[assembly: AssemblyFileVersionAttribute("2.4.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("2.4.0.0")]
+[assembly: AssemblyVersionAttribute("2.5.0.0")]
+[assembly: AssemblyFileVersionAttribute("2.5.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("2.5.0.0")]
 [assembly: AssemblyCompany("mailto:contact@reflexil.net")]
 [assembly: AssemblyProduct("Reflexil")]
-[assembly: AssemblyCopyright("Copyright © 2007-2020 Sebastien Lebreton")]
+[assembly: AssemblyCopyright("Copyright © 2007-2021 Sebastien Lebreton")]
 [assembly: AssemblyTrademark("")]
