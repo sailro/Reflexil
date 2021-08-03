3bf8f3822f255b6c0ea60ce08d7cf474ec0b2361
mskgroup <51709279+mskgroup@users.noreply.github.com>
2021-08-03 08:27:53 +0800

    Bump to 2.6

--- Properties/SolutionVersion.cs
+++ Properties/SolutionVersion.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 
-[assembly: AssemblyVersionAttribute("2.5.1.0")]
-[assembly: AssemblyFileVersionAttribute("2.5.1.0")]
-[assembly: AssemblyInformationalVersionAttribute("2.5.1.0")]
+[assembly: AssemblyVersionAttribute("2.6.0.0")]
+[assembly: AssemblyFileVersionAttribute("2.6.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("2.6.0.0")]
 [assembly: AssemblyCompany("mailto:contact@reflexil.net")]
 [assembly: AssemblyProduct("Reflexil")]
 [assembly: AssemblyCopyright("Copyright © 2007-2021 Sebastien Lebreton")]
